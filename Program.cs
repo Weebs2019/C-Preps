939fa915d086ea1904e023fa440b482afa718e16
Weebs2019 <49344133+Weebs2019@users.noreply.github.com>
2021-06-30 19:06:43 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GetNameOfFiles
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Path");
+            var dirPath = @"" + Console.ReadLine();
+
+            if (Directory.Exists(dirPath))
+            {
+                var path = new DirectoryInfo(dirPath);
+                var files = path.GetFiles();
+            }
+            string[] fileArray = Directory.GetFiles(dirPath);
+            for (int i = 0; i < fileArray.Length; i++)
+            {
+
+                Console.WriteLine(fileArray[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
