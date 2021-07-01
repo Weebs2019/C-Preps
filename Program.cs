93bcb95416343ceabd3c445d2da782a8c0dab230
Weebs2019 <49344133+Weebs2019@users.noreply.github.com>
2021-07-01 13:06:03 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace GetNameOfFiles
         static void Main(string[] args)
         {
             Console.WriteLine("Enter Path");
-            Console.Writeline("Day time");
+            Console.Writeline("Day times");
             var dirPath = @"" + Console.ReadLine();
 
             if (Directory.Exists(dirPath))
